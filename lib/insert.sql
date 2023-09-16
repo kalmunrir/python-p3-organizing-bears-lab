09efe231a4cd208f6f432e7c6ddf336007ef9287
Juan Ortiz-Acosta <jdoa2003@gmail.com>
2023-09-16 15:02:31 -0600

    finished lab

---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES
+    ('Mr. Chocolate', 5, 'M', 'Brown', 'Friendly', TRUE),
+    ('Rowdy', 3, 'M', 'Black', 'Playful', TRUE),
+    ('Tabitha', 4, 'F', 'White', 'Calm', TRUE),
+    ('Sergeant Brown', 6, 'M', 'Brown', 'Aggressive', TRUE),
+    ('Melissa', 2, 'F', 'Honey', 'Shy', TRUE),
+    ('Grinch', 7, 'M', 'Green', 'Grumpy', TRUE),
+    ('Wendy', 5, 'F', 'Brown', 'Energetic', TRUE),
+    (NULL, NULL, NULL, NULL, NULL, TRUE);
